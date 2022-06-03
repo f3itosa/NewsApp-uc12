eb834afdc079f40291b8bd5804794eecb0cca1f1
Eric Batista Feitosa <eric.feitosa@kyndryl.com>
2022-06-02 22:33:43 -0300

    Ajustes e Melhorias

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import {Text,View,ScrollView,TouchableOpacity, Image} from 'react-native';
+import {Text,View,ScrollView,TouchableOpacity, Image, StyleSheet} from 'react-native';
+
+
+import img1 from '../../src/images/img1.png';
+import img2 from '../../src/images/img2.png';
+import img3 from '../../src/images/img3.png';
+
+
+
 
 export default class MyList extends Component {
     state = {
@@ -10,28 +18,30 @@ export default class MyList extends Component {
        hasMore: true
      }
      
-     componentWillMount() { this.getListOfPictures(); };
+     componentDidMount() { this.getListOfData(); };
 
      getListOfData = () => {
         if (this.state.loading) { return; }
         this.setState({ loading: true });
         let newData = [];
         newData.push({
-            title: "Lorem ipsum", 
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in congue risus, non viverra tellus. Nam faucibus ligula non metus ultrices mollis. Cras dolor purus, hendrerit eu eros quis, dignissim eleifend mi. In tincidunt mi in diam egestas congue ac ut purus. Nulla semper libero vitae blandit vehicula.",
-            image: "img1.png",
+            title: "IPhone vai detectar portas e transcrever áudios", 
+            text: "A Apple anunciou novas funções de acessibilidade que serão incluídas nos seus dispositivos físicos (como iPhone e iPad) e versões de sistema operacional ao longo do ano de 2022.",
+            image: require("../images/img1.png"),
             id: this.state.data.length
+           
+
         });
         newData.push({
-            title: "Curabitur vulputate", 
-            text: "Curabitur vulputate enim in lacus imperdiet, a convallis odio posuere. Nulla id ex et purus sodales rutrum non eu eros. Ut consequat est lacus.",
-            image: "img2.png",
+            title: "Linguagem de programação Python se destaca pela facilidade de aprendizado", 
+            text: "A Python ainda é uma das principais linguagens de programação ensinadas em escolas e universidades. Justamente por conta da sua facilidade de aprendizado. E este é mais um indicativo de que o reinado da Python deve continuar, pelo menos por mais alguns anos, ao aprender a linguagem na universidade, os futuros profissionais de tecnologia tendem a continuar a usá-la no mercado de trabalho.",
+            image: require("../images/img2.png"),
             id: this.state.data.length+1
         });
         newData.push({
-            title: "Proin hendrerit", 
-            text: "Proin hendrerit nisl id turpis bibendum, sit amet scelerisque augue elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a blandit sapien.",
-            image: "img3.png",
+            title: "A importância do planejamento no dia a dia de trabalho", 
+            text: "Se você deseja alcançar um objetivo de forma rápida e eficiente, o melhor recurso é utilizar o planejamento. Assim você obtém mais clareza e visão para a tomada de decisão. Não deixe que o tempo passe e seu sonho e suas metas fiquem pelo meio do caminho.",
+            image: require("../images/img3.png"),
             id: this.state.data.length+2
         });
         this.setState({
@@ -44,7 +54,7 @@ export default class MyList extends Component {
     }
 
     isCloseToBottom({ layoutMeasurement, contentOffset, contentSize }) {   
-        return layoutMeasurement.height + contentOffset.y 
+        return layoutMeasurement.height + contentOffset.y
         >= contentSize.height - 50; 
     }
 
@@ -53,9 +63,9 @@ export default class MyList extends Component {
           return ( 
             <TouchableOpacity key={u.id}>
                     <View style={{ padding: 10 }}>
-                        <Image source={require("./src/images/" + u.image)} ></Image>
-                       <Text style={{ fontSize: 15}}>{u.title}</Text>        
-                       <Text>{u.text}</Text>
+                        <Image style={style.image} source={u.image} />
+                       <Text style={style.title}>{u.title}</Text>        
+                       <Text style={style.text}>{u.text}</Text>
                     </View>
              </TouchableOpacity>);
             })
@@ -73,3 +83,23 @@ export default class MyList extends Component {
       }
 
 }
+
+const
+style = StyleSheet.create({
+    image:{
+    width:'100%',
+    height:150,
+    borderRadius:10
+},
+
+title:{
+    fontSize: 15,
+    fontSize:20,
+    marginTop:10
+
+},
+
+text:{
+    marginTop:15
+}
+})
